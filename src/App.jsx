fd29ae88d7f38b9f79f26065eda71d6de588f102
Sanjayan <97502651+Sanjayan-Ganesan@users.noreply.github.com>
2022-04-29 23:35:02 +0530

    commit to main

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import TicTacToe from './Tic_Tac_Toee/TicTacToe'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+     <TicTacToe/>
+    </div>
+  )
+}
+
+export default App
